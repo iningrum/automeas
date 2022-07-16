684cbeac22895b8897d8cc660601a9b6962ebdcb
iningrum <96956031+iningrum@users.noreply.github.com>
2022-07-16 23:08:29 +0200

    Added general layout for launcher. Launcher is supposed to ask basic questions.
    Button styles need improving.

---
+++ automeas-ui/Launcher.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace automeas_ui
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Launcher.xaml
+    /// </summary>
+    public partial class Launcher : Window
+    {
+        public Launcher()
+        {
+            InitializeComponent();
+        }
+    }
+}
